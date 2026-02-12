d2dfc39b7f0a3206d04aa653f992a85a12a4e1c4
Sai-lifestance <TFG.Sai.Sharath@lifestance.com>
2026-02-11 21:45:24 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,9 +5,9 @@ BQ_PROJECT_REVIEWS = "ls-prod-warehouse"
 REVIEWS_TABLE = "marketing_marts.fct_review_responses" 
 
 BQ_PROJECT_SUMMARIES = "ls-raw-dev"
-SUMMARY_TABLE = "ai_generated_outputs.review_summaries_test"
-SENTIMENT_GRADE_TABLE = "ai_generated_outputs.review_sentiment_grades_test"   
-REVIEW_TAGS_TABLE = "ai_generated_outputs.tagged_reviews_test"
+SUMMARY_TABLE = "ai_generated_outputs.review_summaries"
+SENTIMENT_GRADE_TABLE = "ai_generated_outputs.review_sentiment_grades"   
+REVIEW_TAGS_TABLE = "ai_generated_outputs.tagged_reviews"
 
 # ---- OpenAI Config ----
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
@@ -16,4 +16,5 @@ DEFAULT_MODEL = os.getenv("OPENAI_MODEL", "gpt-5-mini")
 # ---- Keywords for Sentiment Grader ----
 
 REVIEW_CATEGORIES = ['Clinical Care and Outcomes', 'Patient Experience', 'Billing', 'Intake Experience', 'Technology']
+
 
